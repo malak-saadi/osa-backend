350b3a1662401995781d7d2247257048396a3348
SoufianeBensetallah1 <s.bensetallah@esi-sba.dz>
2026-04-02 23:10:51 +0100

    note ):

--- note/urls.py
+++ note/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import NoteViewSet
 
-# Create a router and register the NoteViewSet
+# Create a router and register the NoteViewSet t
 router = DefaultRouter()
 router.register(r'', NoteViewSet, basename='note')
 
